b02d9847c2141bb338f9da3a77abea17825c519d
Hamid Achaou <hamidAchaou379@gmail.com>
2023-12-29 15:09:36 +0100

    change version adminlte 3.2.0 in 3.0.0

--- layouts/aside.php
+++ layouts/aside.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Logo de la marque -->
     <a href="../project/index.php" class="brand-link">
-        <img src="../dist/img/gestion-de-projet.png" class="brand-image img-circle elevation-3" alt="Image de groupe">
+        <img src="../adminlte/dist/img/gestion-de-projet.png" class="brand-image img-circle elevation-3" alt="Image de groupe">
         <span class="brand-text font-weight-light text-center">Gestion de Projets</span>
     </a>
 
@@ -12,7 +12,7 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Ajouter des icônes aux liens en utilisant la classe .nav-icon avec Font Awesome ou toute autre bibliothèque d'icônes -->
                 <li class="nav-item">
-                    <a href="../project/index.php" class="nav-link ">
+                    <a href="../adminlte/project/index.php" class="nav-link ">
                         <i class="nav-icon fas fa-table"></i>
                         <p>
                             Projets
--- layouts/script-link.php
+++ layouts/script-link.php
@@ -1,6 +1,6 @@
    <!-- scripts -->
     <!-- jQuery -->
-    <script src="../plugins/jquery/jquery.min.js"></script>
+    <script src="../adminlte/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
     <script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -8,32 +8,32 @@
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="../plugins/chart.js/Chart.min.js"></script>
+    <script src="../adminlte/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="../plugins/sparklines/sparkline.js"></script>
+    <script src="../adminlte/plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="../plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="../plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="../adminlte/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="../adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="../adminlte/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="../plugins/moment/moment.min.js"></script>
+    <script src="../adminlte/plugins/moment/moment.min.js"></script>
     <script src="plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="../adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="../plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="../adminlte/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="../adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="../dist/js/adminlte.js"></script>
+    <script src="../adminlte/dist/js/adminlte.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="../dist/js/demo.js"></script>
+    <script src="../adminlte/dist/js/demo.js"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="../dist/js/pages/dashboard.js"></script>
-    <script src="../dist/js/validationForm.js"></script>
+    <script src="../adminlte/dist/js/pages/dashboard.js"></script>
+    <script src="../adminlte/dist/js/validationForm.js"></script>
     <!-- scripts -->
     <script>
     tinymce.init({
