b3d5d223b0359d7728dc7fec53be4add8102fc67
ADNANLH <adnanharrak5@gmail.com>
2024-05-13 02:06:33 +0100

    maquettage de orthophoniste

---
+++ maquettes/view/maquette-orthophoniste/gestion-consultations/create.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!-- Inclure l'en-tête -->
+<?php include_once "../../layouts/heade.php" ?>
+
+<body class="sidebar-mini" style="height: auto;">
+
+    <div class="wrapper">
+        <!-- Navigation -->
+        <?php include_once "../../layouts/nav.php" ?>
+        <!-- Barre latérale -->
+        <?php include_once "../../layouts/aside.php" ?>
+
+        <div class="content-wrapper" style="min-height: 1302.4px;">
+
+            <div class="content-header">
+            </div>
+
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="card card-info">
+                                <div class="card-header">
+                                    <h3 class="card-title"> <i class="nav-icon fas fa-table"></i></h3>
+                                </div>
+                                <!-- Obtenir le formulaire -->
+
+                                <div class="row">
+
+                                    <div class="col-md-12">
+                                        <div class="card card-default">
+
+                                            <div class="steps">
+                                                <div class="md-stepper-horizontal orange">
+                                                    <div class="md-step done">
+                                                        <div class="md-step-circle"><span>1</span></div>
+                                                        <div class="md-step-title">Rendez-Vous</div>
+                                                        <div class="md-step-bar-left"></div>
+                                                        <div class="md-step-bar-right"></div>
+                                                    </div>
+                                                    <div class="md-step done">
+                                                        <div class="md-step-circle"><span>2</span></div>
+                                                        <div class="md-step-title">Patient</div>
+                                                        <div class="md-step-bar-left"></div>
+                                                        <div class="md-step-bar-right"></div>
+                                                    </div>
+                                                    <div class="md-step active ">
+                                                        <div class="md-step-circle"><span>3</span></div>
+                                                        <div class="md-step-title">Consultation</div>
+                                                        <div class="md-step-bar-left"></div>
+                                                        <div class="md-step-bar-right"></div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+
+                                            <div class="card-body p-0 mt-2">
+                                               
+                                                <div class="form-group col-12">
+                                                    <label> Date Consultation </label>
+                                                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                                                        <input type="text" class="form-control datetimepicker-input" value="04/03/2023" />
+                                                     
+                                                    </div>
+                                                </div>
+                                                <div class="form-group col-12">
+
+                                                    <div class="form-group">
+                                                        <label> Nombre du sicences </label>
+                                                        <input type="number" class="form-control datetimepicker-input" value="4" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex">
+
+                                                <div class="form-group col-md-12">
+                                                    <label>Observation</label>
+                                                    <div id="reservationdate" data-target-input="nearest">
+                                                        <textarea name="Observation" id="summernote" class="form-control" rows="4">Lorem ipsum dolor sit amet consectetur adipisicing elit..</textarea>
+                                                      
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex">
+                                                <div class="form-group col-md-12">
+                                                    <label>Diagnostic</label>
+                                                    <div id="reservationdate" data-target-input="nearest">
+                                                        <textarea name="Remarque" id="summernote2" class="form-control" rows="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, quod cum dolore vel odio cupiditate. Blanditiis debitis voluptas recusandae commodi numquam itaque a.</textarea>
+                                                       
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="d-flex">
+
+                                                <div class="form-group col-md-12">
+                                                    <label>Bilan</label>
+                                                    <div id="reservationdate" data-target-input="nearest">
+                                                        <textarea name="Diagnostic" id="summernote3" class="form-control" rows="4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus natus iusto delectus fugit totam, fuga esse sit reiciendis, maiores non tempore repudiandae! Suscipit !</textarea>
+                                                      
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex">
+                                                <div class="form-group col-md-12">
+                                                    <label>Remarque</label>
+                                                    <div id="reservationdate" data-target-input="nearest">
+                                                        <textarea name="Remarque" id="summernote5" class="form-control" rows="4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati distinctio ut quod eum quis iste porro blanditiis molestiae nesciunt quam dolorum, cupiditate dicta ducimu.</textarea>
+                                                       
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="ml-4 mb-3">
+                                                <a href="./patientInfo.php" class="btn btn-secondary">Presedent</a>
+                                                <a href="./index.php" class="btn btn-info">Enregistrement</a>
+                                            </div>
+                                            </form>
+                                           
+                                        </div>
+                                        <!-- /.card-body -->
+
+                                    </div>
+                                    <!-- /.card -->
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+
+        </section>
+
+    </div>
+    </div>
+    <style>
+        .md-stepper-horizontal {
+            display: table;
+            width: 100%;
+            margin: 0 auto;
+            background-color: #FFFFFF;
+            box-shadow: 0 3px 8px -6px rgba(0, 0, 0, .50);
+        }
+
+        .md-stepper-horizontal .md-step {
+            display: table-cell;
+            position: relative;
+            padding: 24px;
+        }
+
+        .md-stepper-horizontal .md-step:hover,
+        .md-stepper-horizontal .md-step:active {
+            background-color: rgba(0, 0, 0, 0.04);
+        }
+
+        .md-stepper-horizontal .md-step:active {
+            border-radius: 15% / 75%;
+        }
+
+        .md-stepper-horizontal .md-step:first-child:active {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .md-stepper-horizontal .md-step:last-child:active {
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+
+        .md-stepper-horizontal .md-step:hover .md-step-circle {
+            background-color: #757575;
+        }
+
+        .md-stepper-horizontal .md-step:first-child .md-step-bar-left,
+        .md-stepper-horizontal .md-step:last-child .md-step-bar-right {
+            display: none;
+        }
+
+        .md-stepper-horizontal .md-step .md-step-circle {
+            width: 30px;
+            height: 30px;
+            margin: 0 auto;
+            background-color: #999999;
+            border-radius: 50%;
+            text-align: center;
+            line-height: 30px;
+            font-size: 16px;
+            font-weight: 600;
+            color: #FFFFFF;
+        }
+
+
+        /* .md-stepper-horizontal .md-step.done .md-step-circle:before {
+
+        font-weight: 100;
+        content: "\f00c";
+    } */
+
+        .md-stepper-horizontal .md-step.done .md-step-circle *,
+        .md-stepper-horizontal .md-step.editable .md-step-circle * {
+            display: block;
+        }
+
+        .md-stepper-horizontal .md-step.editable .md-step-circle {
+            -moz-transform: scaleX(-1);
+            -o-transform: scaleX(-1);
+            -webkit-transform: scaleX(-1);
+            transform: scaleX(-1);
+        }
+
+        .md-stepper-horizontal .md-step.editable .md-step-circle:before {
+            /* font-family:'FontAwesome'; */
+            font-weight: 100;
+            content: "\f040";
+        }
+
+        .md-stepper-horizontal .md-step .md-step-title {
+            margin-top: 16px;
+            font-size: 16px;
+            font-weight: 600;
+        }
+
+        .md-stepper-horizontal .md-step .md-step-title,
+        .md-stepper-horizontal .md-step .md-step-optional {
+            text-align: center;
+            color: rgba(0, 0, 0, .26);
+        }
+
+        .md-stepper-horizontal .md-step.active .md-step-title {
+            font-weight: 600;
+            color: rgba(0, 0, 0, .87);
+        }
+
+        .md-stepper-horizontal .md-step.active.done .md-step-title,
+        .md-stepper-horizontal .md-step.active.editable .md-step-title {
+            font-weight: 600;
+        }
+
+        .md-stepper-horizontal .md-step .md-step-optional {
+            font-size: 12px;
+        }
+
+        .md-stepper-horizontal .md-step.active .md-step-optional {
+            color: rgba(0, 0, 0, .54);
+        }
+
+        .md-stepper-horizontal .md-step .md-step-bar-left,
+        .md-stepper-horizontal .md-step .md-step-bar-right {
+            position: absolute;
+            top: 36px;
+            height: 1px;
+            border-top: 1px solid #DDDDDD;
+        }
+
+        .md-stepper-horizontal .md-step .md-step-bar-right {
+            right: 0;
+            left: 50%;
+            margin-left: 20px;
+        }
+
+        .md-stepper-horizontal .md-step .md-step-bar-left {
+            left: 0;
+            right: 50%;
+            margin-right: 20px;
+        }
+    </style>
+
+
+
+
+
+
+    <!-- Inclure le pied de page -->
+    <?php include_once "../../layouts/footer.php" ?>
+
+    </div>
+
+    <!-- Inclure le script -->
+    <?php include_once "../../layouts/script-link.php" ?>
+</body>
+
+</html>
---
+++ maquettes/view/maquette-orthophoniste/gestion-sciences/show.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!-- Inclure l'en-tête -->
+<?php include_once "../../layouts/heade.php" ?>
+
+<body class="sidebar-mini" style="height: auto;">
+
+    <div class="wrapper">
+        <!-- Navigation -->
+        <?php include_once "../../layouts/nav.php" ?>
+        <!-- Barre latérale -->
+        <?php include_once "../../layouts/aside.php" ?>
+
+
+
+        <div class="content-wrapper" style="min-height: 1302.4px;">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Détails du consultation</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="./index.php" class="btn btn-default float-right"></i> Retour</a>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="col-sm-12">
+                                        <label for="nom">Bilan:</label>
+                                        <p>Test</p>
+                                    </div>
+
+                                    <!-- Description Field -->
+                                    <div class="col-sm-12">
+                                        <label for="description">Diagnostic:</label>
+                                        <p>Test.</p>
+                                    </div>
+
+                                    <!-- Description Field -->
+                                    <div class="col-sm-12">
+                                        <label for="description">Observation:</label>
+                                        <p>Test</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+
+        <!-- Inclure le pied de page -->
+        <?php include_once "../../layouts/footer.php" ?>
+
+    </div>
+
+    <!-- Inclure le script -->
+    <?php include_once "../../layouts/script-link.php" ?>
+</body>
+
+</html>
